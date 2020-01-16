4d42e4bdfe8b86f07fcb73df9f3cc881725f12b7
Jacob Tate <jacob87608@gmail.com>
2020-01-16 10:30:43 -0600

    started styling the css and updating the comments for the js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -64,176 +64,228 @@ $(document).ready(function () {
     //======================================================================
     //reset function
     $("#reset").on("click", function () {
+        //sets the time back to 30
         timer = 30;
+        //resets the answers
         correct = 0;
         incorrect = 0;
         unanswered = 0;
+        //resets the variable that tells the code what question is next when the time runs out
         timeout = 1;
+        //calls the button1 function
         button1();
+        //starts the timer
         interval();
+        //emptys the end of the game div 
         $("#endgame").empty();
+        //hides the end of game div
         $("#endgame").hide();
+        //hides the reset button
         $("#reset").hide();
     });
 
     //runs when you press the start button
     $("#start").on("click", function () {
+        //starts the timer
         interval();
+        //calls the button1 function
         button1();
+        //shows the timer
         $("#timer").show();
+        //hides the start button
         $("#start").hide();
     });
 
-    //runs when you click one of the first buttons that pop up
+    //the buttons for the first set of questions
     $(".button1").on("click", function () {
-        //gets the button clicked value and stores it in a variable
+        //gets the value of the button clicked and stores it in a variable
         var btn_clicked = $(this).val();
-        //if the button clicked value is 3 you win
+        //if the button clicked value is 3 show the correct1 sereen and add to the correct guesses
         if (btn_clicked === "3") {
             correct++
             $("#correct1").show();
 
         }
-        //else you lose
+        //show the incorrect screen and add to the incorrect1 guesses
         else {
             incorrect++
             $("#incorrect1").show();
         }
+        //hides the question
         $(".title1").hide()
+        //stops the timer
         clearInterval(count_down);
+        //hides the buttons
         $(".button1").hide();
+        //after 3 seconds call the button2 function
         setTimeout(button2, 3 * 1000);
     });
 
 
-    //runs when first buttons are hidden and the second buttons pop up
+    //the buttons for the second set of questions
     $(".button2").on("click", function () {
+        //gets the value of the button clicked and stores it in a variable
         var btn_clicked = $(this).val();
-        //if the button clicked value is 1 you win
+        //if the button clicked value is 1 show the correct2 screen and add the the correct guesses
         if (btn_clicked === "1") {
             correct++
             $("#correct2").show();
         }
-        //else you lose
+        //show the incorrect2 screen and add to the incorrect gusses
         else {
             incorrect++
             $("#incorrect2").show();
         }
+        //hides the questions
         $(".title2").hide()
+        //stops the timer 
         clearInterval(count_down);
+        //hides the buttons
         $(".button2").hide();
+        //calls the button3 function (line:343)
         setTimeout(button3, 3 * 1000);
     });
 
-    //runs when second buttons are hidden and the third buttons pop up
+    //the buttons for the third set of questions
     $(".button3").on("click", function () {
+          //gets the value of the button clicked and stores it in a variable
         var btn_clicked = $(this).val();
-        //if the button clicked value is 1 you win
+        //if the button clicked value is 4 show the correct3 screen and add the the correct guesses
         if (btn_clicked === "4") {
             correct++
             $("#correct3").show();
         }
-        //else you lose
+        //show the incorrect3 screen and add to the incorrect gusses
         else {
             incorrect++
             $("#incorrect3").show();
         }
+        //hides the questions
         $(".title3").hide()
+        //stops the timer 
         clearInterval(count_down);
+         //hides the buttons
         $(".button3").hide();
+         //calls the button3 function 
         setTimeout(button4, 3 * 1000);
     });
 
-    //runs when third buttons are hidden and the fourth buttons pop up
+    //the buttons for the fourth set of questions
     $(".button4").on("click", function () {
+        //gets the value of the button clicked and stores it in a variable
         var btn_clicked = $(this).val();
-        //if the button clicked value is 1 you win
+         //if the button clicked value is 2 show the correct4 screen and add the the correct guesses
         if (btn_clicked === "2") {
             correct++
             $("#correct4").show();
         }
-        //else you lose
+         //show the incorrect4 screen and add to the incorrect gusses
         else {
             incorrect++
             $("#incorrect4").show();
         }
+         //hides the questions
         $(".title4").hide()
+          //stops the timer 
         clearInterval(count_down);
+        //hides the buttons
         $(".button4").hide();
+        //calls the button3 function 
         setTimeout(button5, 3 * 1000);
     });
 
-    //runs when fourth buttons are hidden and the fifth buttons pop up
+    //the buttons for the fifth set of questions
     $(".button5").on("click", function () {
+        //gets the value of the button clicked and stores it in a variable
         var btn_clicked = $(this).val();
-        //if the button clicked value is 1 you win
+         //if the button clicked value is 3 show the correct5 screen and add the the correct guesses
         if (btn_clicked === "3") {
             correct++
             $("#correct5").show();
         }
-        //else you lose
+       //show the incorrect5 screen and add to the incorrect gusses
         else {
             incorrect++
             $("#incorrect5").show();
         }
+        //hides the question
         $(".title5").hide()
+        //stops the timer
         clearInterval(count_down);
+        //hides the buttons
         $(".button5").hide();
+        //calls the button6 function after 3 seconds
         setTimeout(button6, 3 * 1000);
     });
 
-    //runs when fifth buttons are hidden and the sixth buttons pop up
+    //the sixth set of questions
     $(".button6").on("click", function () {
+        //gets the value of the button clicked and stores it in a variable
         var btn_clicked = $(this).val();
-        //if the button clicked value is 1 you win
+        //if the value of the button clicked is 2 show the correct 6 screen and add to the correct guesses
         if (btn_clicked === "2") {
             correct++
             $("#correct6").show();
         }
-        //else you lose
+        //show the incorrect6 screen and add to the incorrect guesses
         else {
             incorrect++
             $("#incorrect6").show();
         }
+        //hides the question
         $(".title6").hide()
+        //stops the timer
         clearInterval(count_down);
+        //hides the buttons
         $(".button6").hide();
+        //calls the button7 function after 3 seconds
         setTimeout(button7, 3 * 1000);
     });
 
-    //runs when sixth buttons are hidden and the seventh buttons pop up
+    //the seventh set of questions
     $(".button7").on("click", function () {
+        //gets the value of the button clicked and stores it in a variable
         var btn_clicked = $(this).val();
-        //if the button clicked value is 1 you win
+        //if the value of the button clicked is 2 show the correct7 screen and add to the correct guesses
         if (btn_clicked === "2") {
             correct++
             $("#correct7").show();
         }
-        //else you lose
+        //show the incorrect7 screen and add to the incorrect guesses
         else {
             incorrect++
             $("#incorrect7").show();
         }
+        //hides the question
         $(".title7").hide()
+        //hides the buttons
         $(".button7").hide();
+        //stops the timer
         clearInterval(count_down);
+        //calls the button8 function after 3 seconds
         setTimeout(button8, 3 * 1000);
     });
-
+//the eighth set of questions
     $(".button8").on("click", function () {
+         //gets the value of the button clicked and stores it in a variable
         var btn_clicked = $(this).val();
+           //if the value of the button clicked is 3 show the correct8 screen and add to the correct guesses
         if (btn_clicked === "3") {
             correct++
             $("#correct8").show();
         }
-        //else you lose
+        //show the incorrect8 screen and add to the incorrect guesses
         else {
             incorrect++
             $("#incorrect8").show();
         }
+        //hides the question
         $(".title8").hide()
+         //hides the buttons
         $(".button8").hide();
+        //stops the timer
         clearInterval(count_down);
+         //calls the endOfGame function after 3 seconds
         setTimeout(endOfGame, 3 * 1000)
     });
 
@@ -242,7 +294,7 @@ $(document).ready(function () {
     //====================================================
     //the interval
     function interval() {
-        timer = 3;
+        timer = 30;
         count_down = setInterval(time, 1000);
     }
     //the timer function
@@ -256,7 +308,6 @@ $(document).ready(function () {
                 $(".button1").hide();
                 $(".title1").hide();
                 $("#timeOut1").show();
-                //setTimeout(clearTimeOut, 3 * 1000);
                 setTimeout(button2, 3 * 1000);
             } else if (timeout === 2) {
                 $(".button2").hide();
